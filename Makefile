c9340b670bb2baad2affb9a4426b49e35ecd8c8d
Tim Watson <watson.timothy@gmail.com>
2014-02-18 16:16:41 +0000

    Even more build system tweaking

--- Makefile
+++ Makefile
@@ -1,9 +1,34 @@
 # CI build
 
-GIT_BASE := git://github.com/haskell-distributed
+GIT_BASE ?= git://github.com/haskell-distributed
+
+# unlike standalone repos, we want our dependencies checked
+# out into the current directory, not a ./build subdirectory
 BUILD_DIR := $(shell pwd)
+
 include build.mk
 
 reset: dist-clean
 	rm -rf $(REPO_NAMES)
+	rm -rf $(REPOS)
+
+install:
+	$(info not installing cloud-haskell.cabal by default)
+	$(info run force-install to override)
+
+force-install:
+	$(CABAL) install
+
+define subcmd
+	- $(MAKE) -C $(1) BUILD_DIR=${PWD} BASE_DIR=${PWD} LOCAL=true BRANCH=${BRANCH} $(2)
+endef
+
+tests: $(REPOS)
+# yes, recursive make is ugly, and this even more so (to keep the output sane...)
+	$(call subcmd,distributed-process-tests install-deps)
+	$(call subcmd,network-transport-tcp install-deps)
+	$(call subcmd,distributed-process-platform install-deps)
+	$(call subcmd,distributed-process-tests test)
+	$(call subcmd,network-transport-tcp test)
+	$(call subcmd,distributed-process-platform test)
 
--- build.mk
+++ build.mk
@@ -7,6 +7,7 @@ ifneq (,$(LOCAL))
 BASE_DIR ?= $(shell dirname `pwd`)
 GIT_BASE ?= $(BASE_DIR)
 SANDBOX ?= $(GIT_BASE)/.cabal-sandbox
+BUILD_DIR ?= $(GIT_BASE)
 else
 SANDBOX ?= $(PWD)/.cabal-sandbox
 GIT_BASE ?= git://github.com/haskell-distributed
@@ -17,9 +18,9 @@ BUILD_DIR ?= $(PWD)/build
 SANDBOX_CONFIG=$(PWD)/cabal.sandbox.config
 TEST_SUITE ?=
 REPO_NAMES=$(shell cat REPOS | sed '/^$$/d')
-REPOS=$(patsubst %,$(BUILD_DIR)/%.repo,$(REPO_NAMES))
+REPOS=$(patsubst %,$(BUILD_DIR)/%/.git/,$(REPO_NAMES))
 CONF=./dist/setup-config
-BUILD_DEPENDS=ensure-dirs $(REPOS) $(SANDBOX) $(CONF)
+BUILD_DEPENDS=$(REPOS) $(SANDBOX) $(CONF)
 
 .PHONY: all
 all: install
@@ -31,10 +32,10 @@ push:
 .PHONY: info
 info:
 	$(info git-base = ${GIT_BASE})
-	$(info branch = ${BRANCH})
-	$(info ghc = ${GHC})
-	$(info cabal = ${CABAL})
-	$(info depends = ${BUILD_DEPENDS})
+	$(info branch   = ${BRANCH})
+	$(info ghc      = ${GHC})
+	$(info cabal    = ${CABAL})
+	$(info depends  = ${BUILD_DEPENDS})
 
 .PHONY: clean
 clean:
@@ -43,9 +44,8 @@ clean:
 .PHONY: dist-clean
 dist-clean: clean
 	rm -rf build
-	$(shell ${CABAL} sandbox delete --sandbox=${SANDBOX})
 	rm -rf $(SANDBOX)
-
+	rm -rf cabal.sandbox.config
 
 .PHONY: build
 compile: $(BUILD_DEPENDS)
@@ -55,39 +55,41 @@ compile: $(BUILD_DEPENDS)
 test: $(BUILD_DEPENDS)
 	$(CABAL) test $(TEST_SUITE) --show-details=always
 
-$(CONF): ensure-dirs
+$(CONF):
 	$(CABAL) configure --enable-tests
 
-$(SANDBOX): $(SANDBOX_CONFIG) install
+$(SANDBOX): $(SANDBOX_CONFIG)
 
 $(SANDBOX_CONFIG):
 	$(CABAL) sandbox init --sandbox=$(SANDBOX)
 
-install: ensure-dirs $(REPOS)
-	$(CABAL) install --enable-tests
+install-deps: $(REPOS)
+	$(CABAL) install --enable-tests --only-dependencies
 
-ifneq (,$(LOCAL))
-%.repo: $(SANDBOX_CONFIG)
-	git --git-dir=$(GIT_BASE)/$(*F)/.git \
-		--work-tree=$(GIT_BASE)/$(*F) \
-		checkout $(BRANCH)
-	$(CABAL) sandbox add-source $(GIT_BASE)/$(*F)
-	touch $@
-else
+install: $(REPOS)
+	$(CABAL) install --force-reinstalls
+
+define repo_name
+	$(lastword $(subst /, ,$1))
+endef
 
 define clone
 	git clone $(GIT_BASE)/$1.git $(BUILD_DIR)/$1
 endef
 
-%.repo: $(SANDBOX_CONFIG)
-	$(call clone,$(*F))
-	git --git-dir=$(@D)/$(*F)/.git \
-		--work-tree=$(@D)/$(*F) \
-		checkout $(BRANCH)
-	$(CABAL) sandbox add-source $(@D)/$(*F) --sandbox=$(SANDBOX)
-	touch $@
+ifneq (,$(LOCAL))
+%.git: $(SANDBOX_CONFIG) $(BUILD_DIR)
+	git --git-dir=$@ \
+            --work-tree=$(BUILD_DIR)/$(strip $(call repo_name,$(@D))) \
+            checkout $(BRANCH)
+	$(CABAL) sandbox add-source $(GIT_BASE)/$(strip $(call repo_name,$(@D)))
+else
+%.git: $(SANDBOX_CONFIG) $(BUILD_DIR)
+	$(call clone,$(strip $(call repo_name,$(@D))))
+	git --git-dir=$@ --work-tree=$(@D) checkout $(BRANCH)
+	$(CABAL) sandbox add-source $(@D) --sandbox=$(SANDBOX)
 endif
 
-ensure-dirs:
+$(BUILD_DIR):
 	mkdir -p $(BUILD_DIR)
 
